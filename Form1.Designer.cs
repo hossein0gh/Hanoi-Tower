f8fe57065cb64c40a0b7cba1e56c15b95ccb9518
Hossein Gh <99269353+hossein0gh@users.noreply.github.com>
2023-04-23 17:47:18 +0330

    Add files via upload
    Hanoi Tower

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace TowersWindows
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DiskCount = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.listMoves = new System.Windows.Forms.ListBox();
+            this.lblMoves = new System.Windows.Forms.Label();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.DiskCount)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DiskCount
+            // 
+            this.DiskCount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(255)))), ((int)(((byte)(216)))));
+            this.DiskCount.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiskCount.Location = new System.Drawing.Point(175, 9);
+            this.DiskCount.Maximum = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            this.DiskCount.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.DiskCount.Name = "DiskCount";
+            this.DiskCount.Size = new System.Drawing.Size(52, 26);
+            this.DiskCount.TabIndex = 0;
+            this.DiskCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.DiskCount.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.DiskCount.ValueChanged += new System.EventHandler(this.DiskCount_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of disks : ";
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(255)))), ((int)(((byte)(216)))));
+            this.btnSolve.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSolve.Location = new System.Drawing.Point(254, 7);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(100, 28);
+            this.btnSolve.TabIndex = 2;
+            this.btnSolve.Text = "Solve";
+            this.btnSolve.UseVisualStyleBackColor = false;
+            this.btnSolve.Click += new System.EventHandler(this.BtnSolve_Click);
+            // 
+            // listMoves
+            // 
+            this.listMoves.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listMoves.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(255)))), ((int)(((byte)(216)))));
+            this.listMoves.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listMoves.FormattingEnabled = true;
+            this.listMoves.ItemHeight = 18;
+            this.listMoves.Location = new System.Drawing.Point(821, 38);
+            this.listMoves.Name = "listMoves";
+            this.listMoves.Size = new System.Drawing.Size(231, 400);
+            this.listMoves.TabIndex = 4;
+            // 
+            // lblMoves
+            // 
+            this.lblMoves.AutoSize = true;
+            this.lblMoves.Font = new System.Drawing.Font("Lucida Calligraphy", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMoves.Location = new System.Drawing.Point(818, 12);
+            this.lblMoves.Name = "lblMoves";
+            this.lblMoves.Size = new System.Drawing.Size(74, 21);
+            this.lblMoves.TabIndex = 5;
+            this.lblMoves.Text = "Moves :";
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Font = new System.Drawing.Font("Lucida Fax", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCounter.Location = new System.Drawing.Point(561, 13);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(226, 18);
+            this.lblCounter.TabIndex = 6;
+            this.lblCounter.Text = "Best solution in : {7} moves";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(15, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 400);
+            this.panel1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe Print", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(216, 446);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(659, 33);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Designed By : Hossein Gholizade and Mohammadreza Khodarahmi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(189)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1064, 483);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.lblMoves);
+            this.Controls.Add(this.listMoves);
+            this.Controls.Add(this.btnSolve);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DiskCount);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tower of hanoi";
+            ((System.ComponentModel.ISupportInitialize)(this.DiskCount)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown DiskCount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.ListBox listMoves;
+        private System.Windows.Forms.Label lblMoves;
+        private System.Windows.Forms.Label lblCounter;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TowersWindows
+{
+    public partial class Form1 : Form
+    {
+        private List<string> moves = new List<string>();
+        private List<Disks> _TowerDisks = new List<Disks>();
+        AnimateView animate = new AnimateView();
+        int _DiskCount = 3;
+        int diskHeight = 30;
+        int baseHeight = 20;//Base is 20 high
+        public Form1()
+        {
+            InitializeComponent();
+            AnimateView.view = panel1;
+            resetPanel();
+        }
+        /// <summary>
+        /// Create disks on Panel
+        /// </summary>
+        private void populateDisks()
+        {
+            int ii = 1;
+            foreach (Disks disk in _TowerDisks)
+            {
+                PictureBox panelBox = disk.box;
+                panelBox.BackColor = colorSelector(disk);
+                disk.width = 200 - (20 * ii);
+                panelBox.Width = disk.width;
+                panelBox.Height = diskHeight;
+                panelBox.Tag = disk.DiskNo;
+                panelBox.BorderStyle = BorderStyle.FixedSingle;
+                Point boxLocation = new Point(getDiskX(disk), (panel1.Height - baseHeight) - (diskHeight * ii++));
+                panelBox.Location = boxLocation;
+                disk.box = panelBox;
+                panel1.Controls.Add(disk.box);
+            }
+        }
+        /// <summary>
+        /// Get X position for Disk 
+        /// </summary>
+        /// <param name="disk">Disk to check</param>
+        /// <returns>X position for Disk</returns>
+        private int getDiskX(Disks disk)
+        {
+            int X = 0;
+            int Peg = 0;
+            switch (disk.peg)
+            {
+                case 'A': Peg = 1; break;
+                case 'B': Peg = 2; break;
+                case 'C': Peg = 3; break;
+            }
+            X = ((panel1.Width / 4) * Peg) - (int)(disk.width / 2);
+
+            return X; 
+        }
+        /// <summary>
+        /// Recreates the Panel
+        /// </summary>
+        private void resetPanel()
+        {
+            //Create pegs for Tower
+            setupTower();
+            panel1.Controls.Clear();
+            _TowerDisks = Enumerable.Range(1, _DiskCount).Select(i => new Disks() { DiskNo = i, peg = 'A', box = new PictureBox() }).OrderByDescending(i => i.DiskNo).ToList();
+            //Place Disks on panel
+            populateDisks();
+            //Set initial text value for least possible moves
+            lblCounter.Text = string.Format("Best possible moves {0}", GetMoveCount(_DiskCount));
+        }
+        /// <summary>
+        /// Get Disk Y position
+        /// </summary>
+        /// <param name="disk">Disk to check</param>
+        /// <returns>Y Position for Disk</returns>
+        private int getDiskY(Disks disk)
+        {
+            int Y = 0;
+            int stackNo = _TowerDisks.Count(x => x.peg == disk.peg);
+            Y = panel1.Height - baseHeight - (diskHeight * stackNo);
+            return Y;
+        }
+        private Color colorSelector(Disks disk)
+        {
+            switch (disk.DiskNo)
+            {
+                case 1: return Color.Red;
+                case 2: return Color.DeepSkyBlue;
+                case 3: return Color.Yellow;
+                case 4: return Color.Green;
+                case 5: return Color.OrangeRed;
+                case 6: return Color.Purple;
+                case 7: return Color.Aqua;
+                default: return Color.HotPink;
+            }
+        }
+        /// <summary>
+        /// Button click event to solve tower
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSolve_Click(object sender, EventArgs e)
+        {
+            resetPanel();
+            btnSolve.Enabled = false;
+            moves.Clear();
+            //listMoves.Items.Clear();                            // Clear List of moves
+            int NumberOfDisks = _DiskCount;                     // Declare number of disks
+            solveTower(NumberOfDisks);
+            listMoves.DataSource = null;
+            listMoves.DataSource = moves;                            // Solve tower
+            btnSolve.Enabled = true;
+        }
+        /// <summary>
+        /// Setup tower values, process tower and display moves required
+        /// </summary>
+        /// <param name="numberOfDisks">Number of disks within the tower scenario</param>
+        private void solveTower(int numberOfDisks)
+        {
+            char startPeg = 'A';                                // start tower in output
+            char endPeg = 'C';                                  // end tower in output
+            char tempPeg = 'B';                                 // temporary tower in output
+            //Solve towers using recursive method
+            solveTowers(numberOfDisks, startPeg, endPeg, tempPeg);      
+        }
+        /// <summary>
+        /// Recursive Method to solve Towers of Hanoi
+        /// </summary>
+        /// <param name="n">Disk to move</param>
+        /// <param name="startPeg">Peg to take disk from</param>
+        /// <param name="endPeg">Peg to move disk to</param>
+        /// <param name="tempPeg">auxiliary peg</param>
+        private void solveTowers(int n, char startPeg, char endPeg, char tempPeg)
+        {
+            if (n > 0)
+            {
+                solveTowers(n - 1, startPeg, tempPeg, endPeg);
+
+                Disks currentDisk = _TowerDisks.Find(x => x.DiskNo == n);
+                currentDisk.peg = endPeg;
+
+                //Animate
+                animate.moveUp(currentDisk.box, 50);
+                if (startPeg < endPeg)//Move Right
+                    animate.moveRight(currentDisk.box, getDiskX(currentDisk));
+                else //move Left
+                    animate.moveLeft(currentDisk.box, getDiskX(currentDisk));
+                animate.moveDown(currentDisk.box, getDiskY(currentDisk));
+
+                //Format line
+                string line = string.Format("Move disk {0} from {1} to {2}", n, startPeg, endPeg);
+                Console.WriteLine(line);
+                moves.Add(line);
+                solveTowers(n - 1, tempPeg, endPeg, startPeg);
+            }
+        }
+        /// <summary>
+        /// get the least amount of moves required to solve the tower
+        /// </summary>
+        /// <param name="numberOfDisks">Total number of disks in tower</param>
+        /// <returns>Number of moves</returns>
+        public static int GetMoveCount(int numberOfDisks)
+        {
+            double numberOfDisks_Double = numberOfDisks;
+            return (int)Math.Pow(2.0, numberOfDisks_Double) - 1;
+        }
+        /// <summary>
+        /// Value changed listener to set informational label
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DiskCount_ValueChanged(object sender, EventArgs e)
+        {
+            _DiskCount = (int)DiskCount.Value;
+            resetPanel();
+        }
+        /// <summary>
+        /// Paint Base to panel1
+        /// </summary>
+        private void setupTower()
+        {
+            panel1.Paint += delegate
+            {
+                setBase();
+            };
+        }
+        /// <summary>
+        /// Draw base and pegs
+        /// </summary>
+        private void setBase()
+        {
+            SolidBrush sb = new SolidBrush(Color.Navy);
+            Graphics g = panel1.CreateGraphics();
+            int topSpacing = 100;
+            int width = 20;
+            //Draw bottom bar
+            g.FillRectangle(sb, 0, panel1.Height - baseHeight, panel1.Width, baseHeight);
+            //Draw Peg 1
+            drawPeg(panel1, g, sb, 1, width, topSpacing);
+            //Draw Peg 2
+            drawPeg(panel1, g, sb, 2, width, topSpacing);
+            //Draw Peg 3
+            drawPeg(panel1, g, sb, 3, width, topSpacing);
+        }
+        /// <summary>
+        /// Draw a peg 
+        /// </summary>
+        /// <param name="canvas">Panel to draw pegs on</param>
+        /// <param name="g">panel.CreateGraphics</param>
+        /// <param name="sb">SolidBrush</param>
+        /// <param name="pegNo">Peg Number 1-3</param>
+        /// <param name="pegWidth">Desired peg width</param>
+        /// <param name="topSpacing">Spacing from the top</param>
+        private void drawPeg(Panel canvas,Graphics g, SolidBrush sb, int pegNo,int pegWidth, int topSpacing)
+        {
+            g.FillRectangle(sb, ((int)(canvas.Width / 4) * pegNo)-(pegWidth/2), topSpacing, pegWidth, canvas.Height - topSpacing);
+        }
+    }
+}
